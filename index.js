0a1349723f16376fab97c50e82a2fd6161d37384
Vincent <vleclerc@inow.fr>
2019-06-05 16:47:41 +0200

    add module

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
 "use strict"
-
-let calculateMonthlyPayment = function (principal, years, rate = 2) {
-    let monthlyRate = 0;
-    if (rate) {
-        monthlyRate = rate / 100 / 12;
-    }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-
-    return { monthlyPayment, monthlyRate };
-};
-
-let calculateAmortization = (principal, years, rate) => {
-    let { monthlyPayment, monthlyRate } = calculateMonthlyPayment(200000, 25, 2);
-    let balance = principal;
-    let amortization = new Array(); // []
-    /* for(let a of amortization){
-        ...
-    }
-    amortization.forEach(a => {
-        ...
-    });*/
-    for (let y = 0; y < years; y++) {
-        let interestY = 0;
-        let principalY = 0;
-        for (let m = 0; m < 12; m++) {
-            let interestM = balance * monthlyRate;
-            let principalM = monthlyPayment - interestM;
-            interestY += interestM;
-            principalY += principalM;
-            balance -= principalM;
-        }
-        amortization.push({ principalY, interestY, balance });
-    }
-    return { monthlyPayment, monthlyRate, amortization };
-};
+// let mortgage = require('./mortgage');(2)
+import { calculateAmortization } from './mortgage'; //(1)
 
 document.getElementById("btnCal").addEventListener('click', () => {
-    let obj = calculateAmortization(200000, 25, 2);
+    let principal = document.getElementById("principal").value;
+    let years = document.getElementById("years").value;
+    let rate = document.getElementById("rate").value;
+    let obj = mortgage.calculateAmortization(principal, years, rate);
     // console.log(obj);
     document.getElementById("monthlyPayment").innerHTML = obj.monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (obj.monthlyRate * 100).toFixed(2);
