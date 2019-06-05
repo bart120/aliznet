323fdb6c10c5deb9b4c8a179b7c309b4d7fa3221
Vincent <vleclerc@inow.fr>
2019-06-05 15:06:38 +0200

    let, object

--- build/index.bundle.js
+++ build/index.bundle.js
@@ -1,12 +1,16 @@
 "use strict";
 
-function test() {
-    var message = "coucou";
-    alert(message);
-}
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years) {
+    var rate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 2;
 
-function test2() {
-    var message = 'test';
-}
+    var monthlyRate = 0;
+    if (rate) {
+        monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
 
-test();
+    return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
+};
+
+var obj = calculateMonthlyPayment(200000, 25, 2);
+console.log(obj);
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+"use strict"
 
-let message = "coucou";
-alert(message);
+let calculateMonthlyPayment = function (principal, years, rate = 2) {
+    let monthlyRate = 0;
+    if (rate) {
+        monthlyRate = rate / 100 / 12;
+    }
+    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+
+    return { monthlyPayment, monthlyRate };
+};
+
+let obj = calculateMonthlyPayment(200000, 25, 2);
+// console.log(obj);
+document.getElementById("monthlyPayment").innerHTML = obj.monthlyPayment.toFixed(2);
+document.getElementById("monthlyRate").innerHTML = (obj.monthlyRate * 100).toFixed(2);
