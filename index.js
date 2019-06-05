f9dc9b2ddb8b1ad7baa1d1f016fd268efc4da063
Vincent <vleclerc@inow.fr>
2019-06-05 15:44:39 +0200

    arrow function, array, foreach

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ let calculateMonthlyPayment = function (principal, years, rate = 2) {
     return { monthlyPayment, monthlyRate };
 };
 
-let obj = calculateMonthlyPayment(200000, 25, 2);
-// console.log(obj);
+let calculateAmortization = (principal, years, rate) => {
+    let { monthlyPayment, monthlyRate } = calculateMonthlyPayment(200000, 25, 2);
+    let balance = principal;
+    let amortization = new Array(); // []
+    /* for(let a of amortization){
+        ...
+    }
+    amortization.forEach(a => {
+        ...
+    });*/
+    for (let y = 0; y < years; y++) {
+        let interestY = 0;
+        let principalY = 0;
+        for (let m = 0; m < 12; m++) {
+            let interestM = balance * monthlyRate;
+            let principalM = monthlyPayment - interestM;
+            interestY += interestM;
+            principalY += principalM;
+            balance -= principalM;
+        }
+        amortization.push({ principalY, interestY, balance });
+    }
+    return { monthlyPayment, monthlyRate, amortization };
+};
+
+let obj = calculateAmortization(200000, 25, 2);
+console.log(obj);
 document.getElementById("monthlyPayment").innerHTML = obj.monthlyPayment.toFixed(2);
 document.getElementById("monthlyRate").innerHTML = (obj.monthlyRate * 100).toFixed(2);
