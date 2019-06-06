ab5f5f0a1e3333270062510329d09b27d129f3ec
Vincent <vleclerc@inow.fr>
2019-06-06 10:34:42 +0200

    add class notation

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 "use strict"
 // let mortgage = require('./mortgage');(2)
-import { calculateAmortization } from './mortgage'; //(1)
+import { Mortgage } from './mortgage'; //(1)
 
 document.getElementById("btnCal").addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let obj = calculateAmortization(principal, years, rate);
+
+    let mortgage = new Mortgage(principal, years, rate);
+    let obj = mortgage.calculateAmortization();
     // console.log(obj);
     document.getElementById("monthlyPayment").innerHTML = obj.monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (obj.monthlyRate * 100).toFixed(2);
     obj.amortization.forEach(x => {
-        console.log(x.principalY);
+        console.log(x);
     });
 });
 
--- mortgage.js
+++ mortgage.js
@@ -1,38 +1,46 @@
 "use strict"
 
-let calculateMonthlyPayment = function (principal, years, rate = 2) {
-    let monthlyRate = 0;
-    if (rate) {
-        monthlyRate = rate / 100 / 12;
+export class Mortgage {
+    constructor(principal, years, rate) {
+        this.principal = principal;
+        this.years = years;
+        this.rate = rate;
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
 
-    return { monthlyPayment, monthlyRate };
-};
-
-export let calculateAmortization = (principal, years, rate) => { //(1)
-    let { monthlyPayment, monthlyRate } = calculateMonthlyPayment(200000, 25, 2);
-    let balance = principal;
-    let amortization = new Array(); // []
-    /* for(let a of amortization){
-        ...
-    }
-    amortization.forEach(a => {
-        ...
-    });*/
-    for (let y = 0; y < years; y++) {
-        let interestY = 0;
-        let principalY = 0;
-        for (let m = 0; m < 12; m++) {
-            let interestM = balance * monthlyRate;
-            let principalM = monthlyPayment - interestM;
-            interestY += interestM;
-            principalY += principalM;
-            balance -= principalM;
+    calculateMonthlyPayment() {
+        let monthlyRate = 0;
+        if (this.rate) {
+            monthlyRate = rate / 100 / 12;
         }
-        amortization.push({ principalY, interestY, balance });
+        let monthlyPayment = this.principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), this.years * 12)));
+
+        return { monthlyPayment, monthlyRate };
     }
-    return { monthlyPayment, monthlyRate, amortization };
-};
+
+    calculateAmortization() { //(1)
+        let { monthlyPayment, monthlyRate } = this.calculateMonthlyPayment(200000, 25, 2);
+        let balance = this.principal;
+        let amortization = new Array(); // []
+        /* for(let a of amortization){
+            ...
+        }
+        amortization.forEach(a => {
+            ...
+        });*/
+        for (let y = 0; y < this.years; y++) {
+            let interestY = 0;
+            let principalY = 0;
+            for (let m = 0; m < 12; m++) {
+                let interestM = balance * monthlyRate;
+                let principalM = monthlyPayment - interestM;
+                interestY += interestM;
+                principalY += principalM;
+                balance -= principalM;
+            }
+            amortization.push({ principalY, interestY, balance });
+        }
+        return { monthlyPayment, monthlyRate, amortization };
+    }
+}
 
 // module.exports = calculateAmortization;(2)
